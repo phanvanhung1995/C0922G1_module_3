719186ae10243628255302075de9535c1c66cedc
phanvanhung1995 <phanvanhung1224@gmail.com>
2022-12-14 11:56:57 +0700

    bai tap

---
+++ mysql/ss1_relational_database/mysql_table.sql
@@ -0,0 +1,19 @@
+create database student_managerment; 
+
+create table class( 
+id int,
+name varchar(50)
+);
+
+insert into class(id,name) value(1, 'hung'), (2, 'hau'), (3, 'vu') ;
+select * from class;
+
+create table teacher( 
+id int,
+name varchar(50),
+age int,
+country varchar(150)
+);
+
+insert into teacher value(1,'congnt', 31, 'da nang'),(2,'chanh',35,'hue');
+select * from teacher ;
