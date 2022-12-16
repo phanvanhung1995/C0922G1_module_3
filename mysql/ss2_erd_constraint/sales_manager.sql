a0081fd6042458dc7ea2d431a90907b4fc20576d
phanvanhung1995 <phanvanhung1224@gmail.com>
2022-12-16 21:57:23 +0700

    bai tap

--- mysql/ss2_erd_constraint/sales_manager.sql
+++ mysql/ss2_erd_constraint/sales_manager.sql
@@ -1,24 +1,24 @@
-create database quan_ly_ban_hang;
-use quan_ly_ban_hang;
+create database sales_manager;
+use sales_manager;
 
 create table customer(
   c_id int primary key, 
   c_name varchar(50), 
-  c_age datetime
+  c_age int
 );
 
 create table oder (
   o_id int primary key, 
   c_id int, 
   o_date datetime, 
-  o_total_primer double, 
+  o_total_primer int, 
   foreign key (c_id) references customer(c_id)
 );
 
 create table product (
   p_id int primary key, 
   p_name varchar(50), 
-  p_price double
+  p_price int
 );
 
 create table oder_detail (
@@ -29,3 +29,5 @@ create table oder_detail (
   foreign key (o_id) references oder (o_id), 
   foreign key (p_id) references product (p_id)
 );
+
+
--- mysql/ss2_erd_constraint/student_management.sql
+++ mysql/ss2_erd_constraint/student_management.sql
@@ -2,34 +2,36 @@ create database student_managerment;
 use student_managerment ;
 
 create table class(
-classID int primary key auto_increment ,
-className varchar(60) not null,
-startDate datetime not null,
+class_id int primary key auto_increment ,
+class_name varchar(60) not null,
+start_date datetime not null,
 status bit 
 );
 
 create table student( 
-studentID int primary key auto_increment,
-studentName varchar(30) not null,
+student_id int primary key auto_increment,
+student_name varchar(30) not null,
 address varchar(50),
 phone varchar(20),
 status bit,
-classID int not null
+class_id int not null
 );
 
 create table subject (
-subID int primary key auto_increment,
-subName varchar(30) not null ,
+sub_id int primary key auto_increment,
+sub_name varchar(30) not null ,
 credit tinyint not null default 1 check ( credit >=1),
 status bit default 1
 );
 
 create table mark (
-markID int not null primary key auto_increment,
-subID int not null unique,
-studentID int not null unique,
+mark_id int not null primary key auto_increment,
+sub_id int not null unique,
+student_id int not null unique,
 mark float default 0 check ( mark between 0 and 100 ),
 exemtimes tinyint default 1,
-foreign key (subID) references subject(subID),
-foreign key (studentID) references student(studentID)
+foreign key (sub_id) references subject(sub_id),
+foreign key (student_id) references student(student_id)
 );
+
+
